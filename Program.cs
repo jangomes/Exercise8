cf8982846ee849b54688dde85537f66b8ae5a09e
Janaina Gomes da Silva <21944@student.dorset-college.ie>
2022-02-05 12:30:35 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        /*
+         Write a console-based application that prompts a user for an hourly pay rate. While the user 
+         enters values less than $5.65 or greater than $49.99, continue to prompt the user. Before the 
+         program ends, display the valid pay rate.
+        */
+        static void Main(string[] args)
+        {
+
+            string a = "y";
+
+            while (a == "y")
+            {
+                Console.WriteLine("Plase input your hourly pay rate: ");
+                double pay = Convert.ToDouble(Console.ReadLine());
+
+                while ((pay < 5.65) || (pay > 49.99))
+                {
+                   Console.WriteLine("\nSorry something is wrong. Please try again: ");
+                   Console.WriteLine("\nPlase input your hourly pay rate: ");
+                   pay = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                }
+
+                Console.WriteLine("\nYou pay rate of "+ pay+ " is valid.");
+
+
+                Console.WriteLine("Do you wanna input another pay rate? \nPlease press y to yes or other to no");
+                a = Console.ReadLine();
+                Console.Clear();
+            }
+            
+        }
+    }
+}
